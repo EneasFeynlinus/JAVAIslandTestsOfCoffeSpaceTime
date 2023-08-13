7f9d8a197fdc572c03613c55c55e73c49f273b9c
Eneas Junior <eneas.silva.jr@gmail.com>
2023-08-12 23:55:14 -0300

    Java Dio Training

---
+++ exemplo_pacotes/src/exemplo_pacotes/Veiculo.java
@@ -0,0 +1,12 @@
+package exemplo_pacotes;
+
+public abstract class Veiculo {
+	private String chassi;
+	public String getChassi() {
+		return chassi;
+	}
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+	public abstract void ligar();
+}
