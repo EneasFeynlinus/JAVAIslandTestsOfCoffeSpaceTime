f22ecd550c647715ed0ba55026a15e8686c6e5fa
Eneas Junior <eneas.silva.jr@gmail.com>
2023-08-22 22:52:21 -0300

    Interface training with, multifunctional example

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/2a/e069d7854541001e1540fc1931ed528e
@@ -0,0 +1,7 @@
+package equipamentos.multifuncional;
+
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultifuncional extends {
+
+}
--- exemplo_pacotes/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
+++ exemplo_pacotes/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -1,7 +1,27 @@
 package equipamentos.multifuncional;
 
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
 import equipamentos.impressora.Impressora;
 
-public class EquipamentoMultifuncional extends {
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {
+
+	@Override
+	public void copiar() {
+		System.out.println("COPIANDO VIA EQUIPAMENTO MULTIFUNCIONL");
+		
+	}
+
+	@Override
+	public void digitalizar() {
+		System.out.println("DIGITALIZANDO VIA EQUIPAMENTO MULTIFUNCIONL");
+		
+	}
+
+	@Override
+	public void imprimir() {
+		System.out.println("IMPRIMINDO VIA EQUIPAMENTO MULTIFUNCIONL");
+		
+	}
 
 }
